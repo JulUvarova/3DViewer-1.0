8211553e813fd990af7d1969196f950b36a78cd1
Anton <antsht@gmail.com>
2025-02-05 22:07:54 +0300

    Cleanup parser and add normalization

--- src/model/obj/obj_parser_test.cc
+++ src/model/obj/obj_parser_test.cc
@@ -1,20 +1,29 @@
 #include <iostream>
+
 #include "obj_data.h"
 
 int main() {
   s21::OBJData obj_data;
-  // obj_data.parse("box_and_cyl.obj");
-  // obj_data.parse("duck.obj");
-  obj_data.parse("megaduck.obj");
+  obj_data.Parse("box_and_cyl.obj");
+  // obj_data.Parse("duck.obj");
+  // obj_data.Parse("megaduck.obj");
 
-  std::cout << "Vertices count: " << obj_data.vertices.size() << std::endl;
-  std::cout << "Objects count: " << obj_data.objects.size() << std::endl;
-  for (const auto& object : obj_data.objects) {
-    std::cout << "Object: " << object.name << std::endl;
-    for (const auto& mesh : object.meshes) {
-      std::cout << "  Group material: " << mesh.material << std::endl;
-      std::cout << "  Faces count: " << mesh.faces.size() << std::endl;
-    }
+  std::cout << obj_data.toString();
+
+  // print top 20 vertices
+  std::cout << "Top 20 vertices:" << std::endl;
+  for (int i = 0; i < 20; i++) {
+    std::cout << obj_data.vertices[i].x << " " << obj_data.vertices[i].y << " "
+              << obj_data.vertices[i].z << std::endl;
+  }
+
+  obj_data.Normalize();
+
+  // print top 20 vertices
+  std::cout << "Top 20 normalized vertices:" << std::endl;
+  for (int i = 0; i < 20; i++) {
+    std::cout << obj_data.vertices[i].x << " " << obj_data.vertices[i].y << " "
+              << obj_data.vertices[i].z << std::endl;
   }
   return 0;
 }
