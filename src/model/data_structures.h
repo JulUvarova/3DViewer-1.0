f4f8c6e3436a9b3a9eea95177c8c810acacdf73f
Anton Shtern <antsht@gmail.com>
2025-02-11 11:49:41 +0300

    Add: transformation matrices factory

--- src/model/data_structures.h
+++ src/model/data_structures.h
@@ -58,37 +58,29 @@ struct Vec4f {
 };
 
 // 4x4 Matrix
-struct Mat4f {
-  std::array<float, 16> m{};
-
-  constexpr float& operator()(int row, int col) { return m[row * 4 + col]; }
-  constexpr const float& operator()(int row, int col) const {
-    return m[row * 4 + col];
-  }
-};
+using Mat4f =  std::array<std::array<float, 4>, 4>;
 
 // Matrix-Matrix Multiplication (External)
 constexpr Mat4f operator*(const Mat4f& a, const Mat4f& b) {
   Mat4f result{};
   for (int row = 0; row < 4; ++row) {
     for (int col = 0; col < 4; ++col) {
-      result(row, col) = a(row, 0) * b(0, col) + a(row, 1) * b(1, col) +
-                         a(row, 2) * b(2, col) + a(row, 3) * b(3, col);
+      result[row][col] = a[row][0] * b[0][col] + a[row][1] * b[1][col] +
+                           a[row][2] * b[2][col] + a[row][3] * b[3][col];
     }
   }
   return result;
 }
-
 // Vector-Matrix Multiplication (External)
 constexpr Vec4f operator*(const Mat4f& mat, const Vec4f& vec) {
-  return {mat(0, 0) * vec.x + mat(0, 1) * vec.y + mat(0, 2) * vec.z +
-              mat(0, 3) * vec.w,
-          mat(1, 0) * vec.x + mat(1, 1) * vec.y + mat(1, 2) * vec.z +
-              mat(1, 3) * vec.w,
-          mat(2, 0) * vec.x + mat(2, 1) * vec.y + mat(2, 2) * vec.z +
-              mat(2, 3) * vec.w,
-          mat(3, 0) * vec.x + mat(3, 1) * vec.y + mat(3, 2) * vec.z +
-              mat(3, 3) * vec.w};
+  return {mat[0][0] * vec.x + mat[0][1] * vec.y + mat[0][2] * vec.z +
+              mat[0][3] * vec.w,
+          mat[1][0] * vec.x + mat[1][1] * vec.y + mat[1][2] * vec.z +
+              mat[1][3] * vec.w,
+          mat[2][0] * vec.x + mat[2][1] * vec.y + mat[2][2] * vec.z +
+              mat[2][3] * vec.w,
+          mat[3][0] * vec.x + mat[3][1] * vec.y + mat[3][2] * vec.z +
+              mat[3][3] * vec.w};
 }
 
 constexpr Vec4f operator*(const Vec4f& vec, const Mat4f& mat) {
--- src/model/math/transform_matrix_builder.h
+++ src/model/math/transform_matrix_builder.h
@@ -1,19 +1,52 @@
 #ifndef TRANSFORMMATRIXBUILDER_H
 #define TRANSFORMMATRIXBUILDER_H
 
+#include "../data_structures.h"
 #include "transform_matrix.h"
 
 namespace s21 {
 
 class TransformMatrixBuilder {
- public: 
-  static TransformMatrix CreateRotationMatrix(float x, float y, float z) {}
-  static TransformMatrix CreateMoveMatrix(float x, float y, float z) {}
-  static TransformMatrix CreateScaleMatrix(float x, float y, float z) {}
-  static TransformMatrix CreateRotationMatrix(float x, float y, float z) {}
+public:
+  static Mat4f TransformMatrixBuilder::CreateRotationMatrix(float x_rot,
+                                                            float y_rot,
+                                                            float z_rot) {
+    const float cosX = std::cos(x_rot);
+    const float sinX = std::sin(x_rot);
+    const Mat4f Rx = {
+        {{1, 0, 0, 0}, {0, cosX, -sinX, 0}, {0, sinX, cosX, 0}, {0, 0, 0, 1}}};
 
- private:
+    const float cosY = std::cos(y_rot);
+    const float sinY = std::sin(y_rot);
+    const Mat4f Ry = {
+        {{cosY, 0, sinY, 0}, {0, 1, 0, 0}, {-sinY, 0, cosY, 0}, {0, 0, 0, 1}}};
+
+    const float cosZ = std::cos(z_rot);
+    const float sinZ = std::sin(z_rot);
+    const Mat4f Rz = {
+        {{cosZ, -sinZ, 0, 0}, {sinZ, cosZ, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}};
+
+    return Rz * Ry * Rx;
+  }
+
+  static Mat4f TransformMatrixBuilder::CreateMoveMatrix(float x_tr, float y_tr,
+                                                        float z_tr) {
+    return {{{1.0f, 0.0f, 0.0f, x_tr},
+             {0.0f, 1.0f, 0.0f, y_tr},
+             {0.0f, 0.0f, 1.0f, z_tr},
+             {0.0f, 0.0f, 0.0f, 1.0f}}};
+  }
+  static Mat4f TransformMatrixBuilder::CreateScaleMatrix(float x_scale,
+                                                         float y_scale,
+                                                         float z_scale) {
+    return {{{x_scale, 0.0f, 0.0f, 0.0f},
+             {0.0f, y_scale, 0.0f, 0.0f},
+             {0.0f, 0.0f, z_scale, 0.0f},
+             {0.0f, 0.0f, 0.0f, 1.0f}}};
+  }
+
+private:
   TransformMatrixBuilder() = delete;
 };
-}  // namespace s21
-#endif  // TRANSFORMMATRIXBUILDER_H
+} // namespace s21
+#endif // TRANSFORMMATRIXBUILDER_H
